f89007c5a923fbd74ac29c37f5dff54703ab8803
Jonas Platte <jplatte+git@posteo.de>
2020-06-21 23:38:47 +0200

    Add basic command line options

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,5 @@
+use clap::{App, Arg};
+
 pub struct Config {
     pub normal_deps: bool,
     pub build_deps: bool,
@@ -5,8 +7,40 @@ pub struct Config {
     pub target_deps: bool,
 }
 
-pub fn parse_options() -> Config {
-    // TODO
+pub fn parse_options() -> anyhow::Result<Config> {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .arg(Arg::with_name("all_deps").long("all-deps").help(
+            "Include all dependencies in the graph \
+             (shorthand for --build-deps --dev-deps --target-deps)",
+        ))
+        .arg(
+            Arg::with_name("build_deps")
+                .long("build-deps")
+                .help("Include build-dependencies in the graph"),
+        )
+        .arg(
+            Arg::with_name("dev_deps")
+                .long("dev-deps")
+                .help("Include dev-dependencies in the graph"),
+        )
+        .arg(
+            Arg::with_name("target_deps")
+                .long("target-deps")
+                .help("Include cfg() dependencies in the graph"),
+        )
+        .arg(
+            Arg::with_name("no_normal_deps")
+                .long("no-normal-deps")
+                .help("Don't include normal dependencies in the graph"),
+        )
+        .get_matches();
 
-    Config { normal_deps: true, build_deps: false, dev_deps: false, target_deps: false }
+    let all_deps = matches.is_present("all_deps");
+    let normal_deps = !matches.is_present("no_normal_deps");
+    let build_deps = all_deps || matches.is_present("build_deps");
+    let dev_deps = all_deps || matches.is_present("dev_deps");
+    let target_deps = all_deps || matches.is_present("target_deps");
+
+    Ok(Config { normal_deps, build_deps, dev_deps, target_deps })
 }
